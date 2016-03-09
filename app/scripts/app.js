2f0384f3871a6b60e9e1ebfa9aa74c483c0ac281
Doug Irvine <dugger14@gmail.com>
2016-03-08 23:01:59 -0500

    Form submit changed to dynamic array.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,18 @@
 var activitiesApp  =  angular.module('activitiesApp', ["firebase"]);
 
 activitiesApp.controller("activitiesController", function($scope, $firebaseArray) {
-  var ref = new Firebase("https://torrid-torch-930814.firebaseio.com/");
+/*  var ref = new Firebase("https://torrid-torch-9308.firebaseio.com/");
+*/  
+  var ref = new Firebase("https://f0gccg5jh1t14.firebaseio-demo.com/");
   
   // create a synchronized array
   $scope.activities = $firebaseArray(ref);
   
+  var newActivity = [];
     // add new items to the array
   // the message is automatically added to our Firebase database!
   $scope.addA = function() {
-    $scope.activities.$add({
-        contact : $scope.contact,
-        partner : $scope.partner,
-        notes : $scope.notes,
-        date : $scope.date,
-        type : $scope.type
-        });
+    $scope.activities.$add(this.newActivity);
       $("#formContainer").fadeOut();
       $("#form1").fadeOut();
       $("#mask").css("background-color","");
